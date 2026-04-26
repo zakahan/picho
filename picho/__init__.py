2c0fa01c265bf9a4489272484c59d666e542ab3b
hanzhi.421 <hanzhi.421@bytedance.com>
2026-04-26 23:56:10 +0800

    chore(release): bump version to 0.1.4

--- picho/__init__.py
+++ picho/__init__.py
@@ -2,6 +2,6 @@ from .provider import get_model
 from .agent.agent import Agent
 from .runner.runner import Runner
 
-__version__ = "0.1.2"
+__version__ = "0.1.4"
 
 __all__ = ["get_model", "Agent", "Runner", "__version__"]
