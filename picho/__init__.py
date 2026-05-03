7f3f46a178994364fff7c61d5130adc6e68429e5
hanzhi.421 <hanzhi.421@bytedance.com>
2026-05-03 12:01:12 +0800

    chore: bump version to 0.1.7

--- picho/__init__.py
+++ picho/__init__.py
@@ -2,6 +2,6 @@ from .provider import get_model
 from .agent.agent import Agent
 from .runner.runner import Runner
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 __all__ = ["get_model", "Agent", "Runner", "__version__"]
